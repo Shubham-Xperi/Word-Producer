308b2d3b8266b2d37153de9861b8fc0e115a16c3
Shubham Prasad <shubham.prasad@xperi.com>
2023-12-17 15:52:52 +0530

    2. Rabasing repo

---
+++ src/main/java/com/shubhogurpreet/wordproducer/WordProducerApplication.java
@@ -0,0 +1,13 @@
+package com.shubhogurpreet.wordproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordProducerApplication.class, args);
+	}
+
+}
